1619a7c3398bb2d3a8f557976f1a340ee28782ae
raghavyuva <vikramnbhat15@gmail.com>
2025-03-04 21:15:48 +0530

    Add : docstring for functions

--- api/internal/features/domain/service/update_domain.go
+++ api/internal/features/domain/service/update_domain.go
@@ -4,7 +4,12 @@ import (
 	"github.com/raghavyuva/nixopus-api/internal/features/domain/types"
 )
 
-func (s *DomainsService) UpdateDomain(newName string,userID string,domainID string) (types.CreateDomainResponse, error) {
+// UpdateDomain updates an existing domain in the application.
+//
+// It takes a domain ID, a userID and a new name and updates the domain in the storage layer.
+// If the domain does not exist, it returns ErrDomainNotFound.
+// If any error occurs while updating the domain, it returns the error.
+func (s *DomainsService) UpdateDomain(newName string, userID string, domainID string) (types.CreateDomainResponse, error) {
 	existing_domain, err := s.storage.GetDomain(domainID)
 	if err != nil {
 		return types.CreateDomainResponse{}, err
@@ -13,7 +18,7 @@ func (s *DomainsService) UpdateDomain(newName string,userID string,domainID stri
 		return types.CreateDomainResponse{}, types.ErrDomainNotFound
 	}
 
-	if err := s.storage.UpdateDomain(existing_domain.ID.String(),newName); err != nil {
+	if err := s.storage.UpdateDomain(existing_domain.ID.String(), newName); err != nil {
 		return types.CreateDomainResponse{}, err
 	}
 
--- api/internal/features/github-connector/service/update_connector.go
+++ api/internal/features/github-connector/service/update_connector.go
@@ -2,7 +2,16 @@ package service
 
 import "fmt"
 
-func (c *GithubConnectorService) UpdateGithubConnectorRequest(InstallationID string,UserID string) error {
+// UpdateGithubConnectorRequest updates the GitHub connector request for the given user ID.
+//
+// The method first retrieves all GitHub connectors associated with the user ID.
+// If no connectors are found, the method simply returns.
+//
+// Otherwise, the method takes the ID of the first connector and updates the
+// associated GitHub app ID with the provided InstallationID.
+//
+// If any errors occur during the update process, the method returns the error.
+func (c *GithubConnectorService) UpdateGithubConnectorRequest(InstallationID string, UserID string) error {
 	connector, err := c.storage.GetAllConnectors(UserID)
 	if err != nil {
 		fmt.Println(err)
