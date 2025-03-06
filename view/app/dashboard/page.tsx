877a5792214bca68d38d548ae67f44b9b2cfd17a
raghavyuva <vikramnbhat15@gmail.com>
2025-03-06 18:46:12 +0530

    create and list all the repositories with pagination

--- view/app/dashboard/page.tsx
+++ view/app/dashboard/page.tsx
@@ -1,27 +1,76 @@
 'use client';
+
 import React from 'react';
 import GitHubAppSetup from './components/github-connector/github-app-setup';
-import { useGetAllGithubConnectorQuery } from '@/redux/services/connector/githubConnectorApi';
 import ListRepositories from './components/github-repositories/list-repositories';
-import Loading from '@/components/ui/loading';
+import AppItem, { AppItemSkeleton } from './components/application';
+import useGetDeployedApplications from './hooks/use_get_deployed_applications';
+import PaginationWrapper from '@/components/pagination';
+import { DahboardUtilityHeader } from '@/components/dashboard-page-header';
+import { Application } from '@/redux/types/applications';
 
 function page() {
   const {
-    data: connectors,
-    refetch: GetGithubConnectors,
-    isLoading
-  } = useGetAllGithubConnectorQuery();
+    connectors,
+    GetGithubConnectors,
+    isLoading,
+    applications,
+    GetApplications,
+    isLoadingApplications,
+    searchTerm,
+    handleSearchChange,
+    onSortChange,
+    sortOptions,
+    sortConfig,
+    handlePageChange,
+    currentPage,
+    totalPages
+  } = useGetDeployedApplications();
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  const showApplications = applications?.length > 0 || isLoadingApplications;
 
   return (
     <div className="container mx-auto py-6 space-y-8 max-w-4xl">
-      {connectors?.length === 0 ? (
-        <GitHubAppSetup GetGithubConnectors={GetGithubConnectors} />
+      {!showApplications ? (
+        connectors?.length === 0 ? (
+          <GitHubAppSetup GetGithubConnectors={GetGithubConnectors} />
+        ) : (
+          <ListRepositories />
+        )
       ) : (
-        <ListRepositories />
+        <>
+          <DahboardUtilityHeader<Application>
+            searchTerm={searchTerm}
+            handleSearchChange={handleSearchChange}
+            sortConfig={sortConfig}
+            onSortChange={onSortChange}
+            sortOptions={sortOptions}
+            label="Deployed Applications"
+            className="mt-5 mb-5"
+          />
+
+          {isLoading || isLoadingApplications ? (
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+              <AppItemSkeleton />
+              <AppItemSkeleton />
+              <AppItemSkeleton />
+            </div>
+          ) : (
+            <>
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                {applications && applications.map((app: any) => <AppItem key={app.id} {...app} />)}
+              </div>
+
+              <div className="mt-8 flex justify-center">
+                <PaginationWrapper
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+            </>
+          )}
+        </>
       )}
     </div>
   );
