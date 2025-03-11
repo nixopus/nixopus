29b380e23907d9868a3754e61d09738edb6daa28
raghavyuva <vikramnbhat15@gmail.com>
2025-03-11 21:54:44 +0530

    docs and landing folders yarn changes

--- view/redux/api-conf.ts
+++ view/redux/api-conf.ts
@@ -40,5 +40,14 @@ export enum GITHUB_CONNECTOR {
 
 export enum DEPLOY {
   GET_APPLICATIONS = 'v1/deploy/applications',
-  CREATE_APPLICATION = 'v1/deploy/application'
+  CREATE_APPLICATION = 'v1/deploy/application',
+  GET_APPLICATION = 'v1/deploy/application'
+}
+
+export enum SOCKET_EVENTS {
+  MONITOR_APPLICATION_DEPLOYMENT = 'monitor_application_deployment'
+}
+
+export enum SOCKET_ACTIONS {
+  SUBSCRIBE = 'subscribe'
 }
