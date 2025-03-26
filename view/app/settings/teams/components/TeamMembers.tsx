e966c28cefb92ab804c5712336c62084557b9f30
raghavyuva <vikramnbhat15@gmail.com>
2025-03-26 20:11:24 +0530

    remove logs from migrations

--- view/app/settings/teams/components/TeamMembers.tsx
+++ view/app/settings/teams/components/TeamMembers.tsx
@@ -39,11 +39,11 @@ interface TeamMembersProps {
 
 function TeamMembers({ users, handleRemoveUser, getRoleBadgeVariant }: TeamMembersProps) {
   const loggedInUser = useAppSelector((state) => state.auth.user);
-
+  const activeOrganization = useAppSelector((state) => state.user.activeOrganization);
   const {
     canUpdate: canUpdateUser,
     canDelete: canDeleteUser
-  } = useResourcePermissions(loggedInUser, "user");
+  } = useResourcePermissions(loggedInUser, "user", activeOrganization?.id);
 
   return (
     <Card>
@@ -58,9 +58,9 @@ function TeamMembers({ users, handleRemoveUser, getRoleBadgeVariant }: TeamMembe
               <TableHead>User</TableHead>
               <TableHead>Role</TableHead>
               <TableHead>Permissions</TableHead>
-              {(canUpdateUser || canDeleteUser) && (
+              {/* {(canUpdateUser || canDeleteUser) && (
                 <TableHead className="text-right">Actions</TableHead>
-              )}
+              )} */}
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -95,7 +95,7 @@ function TeamMembers({ users, handleRemoveUser, getRoleBadgeVariant }: TeamMembe
                     ))}
                   </div>
                 </TableCell>
-                {(canUpdateUser || canDeleteUser) && loggedInUser.id !== user.id && (
+                {/* {(canUpdateUser || canDeleteUser) && loggedInUser.id !== user.id && (
                   <TableCell className="text-right">
                     <DropdownMenu>
                       <DropdownMenuTrigger asChild>
@@ -124,7 +124,7 @@ function TeamMembers({ users, handleRemoveUser, getRoleBadgeVariant }: TeamMembe
                       </DropdownMenuContent>
                     </DropdownMenu>
                   </TableCell>
-                )}
+                )} */}
               </TableRow>
             ))}
           </TableBody>
