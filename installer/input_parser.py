99b6cdb079adc7a9ada72995c8d4559fa067930c
raghavyuva <vikramnbhat15@gmail.com>
2025-05-31 10:22:59 +0530

    fix: import order and use self.validation in domain from arg function

--- installer/input_parser.py
+++ installer/input_parser.py
@@ -1,8 +1,10 @@
 import argparse
-from installer.validation import Validation
 import secrets
 import string
 import sys
+
+from installer.validation import Validation
+
 
 class InputParser:
     def __init__(self):
@@ -44,10 +46,9 @@ class InputParser:
     
     def get_domains_from_args(self, args):
         if args.api_domain and args.app_domain:
-            validation = Validation()
             try:
-                validation.validate_domain(args.api_domain)
-                validation.validate_domain(args.app_domain)
+                self.validation.validate_domain(args.api_domain)
+                self.validation.validate_domain(args.app_domain)
                 return {
                     "api_domain": args.api_domain,
                     "app_domain": args.app_domain,
@@ -61,44 +62,40 @@ class InputParser:
         Get the admin credentials from the command line arguments
         """
         if args.email or args.password:
-            validation = Validation()
-            
             # If both email and password are provided, validate them and we're good to go
             if args.email and args.password:
-                validation.validate_email(args.email)
-                validation.validate_password(args.password)
+                self.validation.validate_email(args.email)
+                self.validation.validate_password(args.password)
                 return args.email, args.password
             # If only email is provided, validate it and ask for password
             elif args.email:
-                validation.validate_email(args.email)
+                self.validation.validate_email(args.email)
                 password = input("Please enter the password for the admin(generates a strong password if left blank): ")
                 if not password:
                     password = self.generate_strong_password()
-                validation.validate_password(password)
+                self.validation.validate_password(password)
                 return args.email, password
             # If only password is provided, validate it and ask for email
             elif args.password:
-                validation.validate_password(args.password)
+                self.validation.validate_password(args.password)
                 while True:
                     email = input("Please enter the email for the admin: ")
                     try:
-                        validation.validate_email(email)
+                        self.validation.validate_email(email)
                         return email, args.password
                     except SystemExit:
                         print("Please enter a valid email address")
                         continue
         return None, None
     
-    # will be used if no args are provided
     def ask_admin_credentials(self):
         """
         Ask for admin credentials
         """
-        validation = Validation()
         while True:
             email = input("Please enter the email for the admin: ")
             try:
-                validation.validate_email(email)
+                self.validation.validate_email(email)
                 break
             except SystemExit:
                 print("Please enter a valid email address")
@@ -107,8 +104,8 @@ class InputParser:
         password = input("Please enter the password for the admin(generates a strong password if left blank): ")
         if not password:
             password = self.generate_strong_password()
-        validation.validate_password(password)
-        return email, password 
+        self.validation.validate_password(password)
+        return email, password
     
     # def ask_for_domain(self):
     #     """
