562af31d36a2f328a4375923c64852451ae65332
raghavyuva <vikramnbhat15@gmail.com>
2025-08-15 00:30:06 +0530

    feat: add fetching branches for repository during self hosting a new application - less error prone and improved accessibility

---
+++ api/internal/features/github-connector/controller/get_repository_branches.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/go-fuego/fuego"
+	"github.com/raghavyuva/nixopus-api/internal/features/logger"
+	"github.com/raghavyuva/nixopus-api/internal/utils"
+
+	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
+)
+
+type GetGithubRepositoryBranchesRequest struct {
+	RepositoryName string `json:"repository_name" validate:"required"`
+}
+
+func (c *GithubConnectorController) GetGithubRepositoryBranches(f fuego.ContextWithBody[GetGithubRepositoryBranchesRequest]) (*shared_types.Response, error) {
+	w, r := f.Response(), f.Request()
+	user := utils.GetUser(w, r)
+
+	if user == nil {
+		return nil, fuego.HTTPError{
+			Err:    nil,
+			Status: http.StatusUnauthorized,
+		}
+	}
+
+	body, err := f.Body()
+	if err != nil {
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusBadRequest,
+		}
+	}
+
+	branches, err := c.service.GetGithubRepositoryBranches(user.ID.String(), body.RepositoryName)
+	if err != nil {
+		c.logger.Log(logger.Error, err.Error(), "")
+		return nil, fuego.HTTPError{
+			Err:    err,
+			Status: http.StatusInternalServerError,
+		}
+	}
+
+	return &shared_types.Response{
+		Status:  "success",
+		Message: "Branches fetched successfully",
+		Data:    branches,
+	}, nil
+}
---
+++ api/internal/features/github-connector/service/get_repository_branches.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/raghavyuva/nixopus-api/internal/features/logger"
+	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
+)
+
+func (c *GithubConnectorService) GetGithubRepositoryBranches(user_id string, repository_name string) ([]shared_types.GithubRepositoryBranch, error) {
+	connectors, err := c.storage.GetAllConnectors(user_id)
+	if err != nil {
+		c.logger.Log(logger.Error, err.Error(), "")
+		return nil, err
+	}
+
+	if len(connectors) == 0 {
+		c.logger.Log(logger.Error, "No connectors found for user", user_id)
+		return nil, nil
+	}
+
+	installation_id := connectors[0].InstallationID
+
+	jwt := GenerateJwt(&connectors[0])
+
+	accessToken, err := c.getInstallationToken(jwt, installation_id)
+	if err != nil {
+		c.logger.Log(logger.Error, fmt.Sprintf("Failed to get installation token: %s", err.Error()), "")
+		return nil, err
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.github.com/repos/%s/branches", repository_name), nil)
+	if err != nil {
+		c.logger.Log(logger.Error, err.Error(), "")
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", accessToken))
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	req.Header.Set("User-Agent", "nixopus")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		c.logger.Log(logger.Error, err.Error(), "")
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		c.logger.Log(logger.Error, fmt.Sprintf("GitHub API error: %s - %s", resp.Status, string(bodyBytes)), "")
+		return nil, fmt.Errorf("GitHub API error: %s", resp.Status)
+	}
+
+	var branches []shared_types.GithubRepositoryBranch
+
+	err = json.NewDecoder(resp.Body).Decode(&branches)
+	if err != nil {
+		c.logger.Log(logger.Error, err.Error(), "")
+		return nil, err
+	}
+
+	return branches, nil
+}
