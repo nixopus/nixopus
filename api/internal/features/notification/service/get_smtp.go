c814b85127c82fb7b426cf9d2affa7dc1bf4f171
Manaswa S <mnswamain@gmail.com>
2025-09-09 18:58:40 +0530

    feat(notification): handle smtpConfigs not found and undefined smtpConfigs iss#377

--- api/internal/features/notification/service/get_smtp.go
+++ api/internal/features/notification/service/get_smtp.go
@@ -1,9 +1,8 @@
 package service
 
 import (
-	"fmt"
-
 	"github.com/raghavyuva/nixopus-api/internal/features/logger"
+	"github.com/raghavyuva/nixopus-api/internal/features/notification"
 	shared_types "github.com/raghavyuva/nixopus-api/internal/types"
 )
 
@@ -27,7 +26,7 @@ func (s *NotificationService) GetSmtp(ID string, organizationID string) (*shared
 	}
 
 	if len(smtpConfigs) == 0 {
-		return nil, fmt.Errorf("no SMTP configurations found for organization ID=%s", organizationID)
+		return nil, notification.ErrSMTPConfigNotFound
 	}
 
 	return &smtpConfigs[0], nil
